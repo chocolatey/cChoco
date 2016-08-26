651d86010b5e73cb61dbe6cb0021c0122f40756f
javydekoning <javydekoning@gmail.com>
2016-08-26 16:16:59 +0200

    Adjusting AppVeyor tests for Win2012r2

--- AppVeyor/AppVeyorInstall.ps1
+++ AppVeyor/AppVeyorInstall.ps1
@@ -13,11 +13,21 @@ Write-Host "Installed NuGet version '$($pkg.version)'"
 #---------------------------------# 
 # Install Modules                 # 
 #---------------------------------# 
-Write-Host 'Installing PSScriptAnalyzer,Pester and xDSCResourceDesigner'
-Install-Module -Name 'PSScriptAnalyzer','Pester','xDSCResourceDesigner' -Repository PSGallery -Force -SkipPublisherCheck -ErrorAction Stop
+$RequiredModules = 'PSScriptAnalyzer','Pester','xDSCResourceDesigner'
+Install-Module -Name $RequiredModules -Repository PSGallery -Force -ErrorAction Stop
 
 #---------------------------------# 
 # Update PSModulePath             # 
 #---------------------------------# 
 Write-Host 'Updating PSModulePath for DSC resource testing'
 $env:PSModulePath = $env:PSModulePath + ";" + "C:\projects"
+
+#---------------------------------# 
+# Validate                        # 
+#---------------------------------# 
+$InstalledModules = Get-Module -Name $RequiredModules -ListAvailable
+if ($InstalledModules.count -lt $RequiredModules.Count) { 
+  throw "Required modules are missing."
+} else {
+  Write-Host 'All modules required found' -ForegroundColor Green
+}
